772ee62aa6c9054939adda823c20bc5cc09053f7
Ashishxo <dhiwarashish4@gmail.com>
2024-02-22 14:51:33 +0530

    multivariable grad descent

--- Linear Regression/04multipleLinearRegression.py
+++ Linear Regression/04multipleLinearRegression.py
@@ -2,89 +2,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy, math
 
-def compute_cost(X, y, w, b): 
+def compute_cost(X, y, w, b):
     m = X.shape[0]
-    cost = 0.0
-    for i in range(m):                                
-        f_wb_i = np.dot(X[i], w) + b           #(n,)(n,) = scalar (see np.dot)
-        cost = cost + (f_wb_i - y[i])**2       #scalar
-    cost = cost / (2 * m)                      #scalar    
+    cost=0.0
+
+    for i in range(m):
+        f_wb_i = np.dot(w, X[i]) + b
+        cost += (f_wb_i - y[i]) ** 2
+
+    cost /= 2 * m
     return cost
 
 
+def gradient(X, y, w, b):
+    m, n = X.shape
 
-def predict(x, w, b): 
-    p = np.dot(x, w) + b     
-    return p  
+    dj_dw = np.zeros(n)
+    dj_db= 0.
 
+    for i in range(m):
+        error = (np.dot(X[i], w) + b) - y[i]
+        for j in range(n):
+            dj_dw[j] += error * X[i, j]
+        dj_db += error
 
-def compute_gradient(X, y, w, b): 
-    m,n = X.shape           #(number of examples, number of features)
-    dj_dw = np.zeros((n,))
-    dj_db = 0.
+    dj_dw /= m
+    dj_db /= m
 
-    for i in range(m):                             
-        err = (np.dot(X[i], w) + b) - y[i]   
-        for j in range(n):                         
-            dj_dw[j] = dj_dw[j] + err * X[i, j]    
-        dj_db = dj_db + err                        
-    dj_dw = dj_dw / m                                
-    dj_db = dj_db / m                                
-        
-    return dj_db, dj_dw
+    return dj_dw, dj_db
 
 
-def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
-    
-    # An array to store cost J and w's at each iteration primarily for graphing later
+def compute_gradient(X, y, w_in, b_in, cost_function, gradFunction, alpha, iterations):
+    w_final = copy.deepcopy(w_in)
+    b_final = b_in
     J_history = []
-    w = copy.deepcopy(w_in)  #avoid modifying global w within function
-    b = b_in
-    
-    for i in range(num_iters):
+    I_history = []
+    for i in range(iterations):
+        dj_dw, dj_db = gradFunction(X, y, w_final, b_final)
 
-        # Calculate the gradient and update the parameters
-        dj_db,dj_dw = gradient_function(X, y, w, b)   ##None
-
-        # Update Parameters using w, b, alpha and gradient
-        w = w - alpha * dj_dw               ##None
-        b = b - alpha * dj_db               ##None
-      
-        # Save cost J at each iteration
-        if i<100000:      # prevent resource exhaustion 
-            J_history.append( cost_function(X, y, w, b))
-
-        # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(num_iters / 10) == 0:
-            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")
         
-    return w, b, J_history #return final w,b and J history for graphing
 
+        w_final = w_final - alpha * dj_dw
+        b_final = b_final - alpha * dj_db
 
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(X, y, w_final, b_final))
+            I_history.append(i)
 
+    return w_final, b_final, J_history, I_history
 
+    
+        
 
 
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
 
 
-b_init = 785.1811367994083
-w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
-
-print(compute_cost(X_train, y_train, w_init, b_init))
+b_init = 0
+w_init = np.array([ 0, 0, 0, 0])
 
 
-w_final, b_final, J_hist = gradient_descent(X_train, y_train, w_init, b_init,
-                                                    compute_cost, compute_gradient, 
-                                                    5.0e-7, 1000)
-print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+w, b, jHist, iHist = compute_gradient(X_train, y_train, w_init, b_init, compute_cost, gradient, 5.2e-7, 1000) 
+
+print(w)
+print(b)
 
 
-fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
-ax1.plot(J_hist)
-ax2.plot(100 + np.arange(len(J_hist[100:])), J_hist[100:])
-ax1.set_title("Cost vs. iteration");  ax2.set_title("Cost vs. iteration (tail)")
-ax1.set_ylabel('Cost')             ;  ax2.set_ylabel('Cost') 
-ax1.set_xlabel('iteration step')   ;  ax2.set_xlabel('iteration step') 
+plt.plot(iHist, jHist)
 plt.show()
